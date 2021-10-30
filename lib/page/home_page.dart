01bb58ed0b24bd842dc0491953a6ccc286df9987
Mustafa Karakaş <mustafa.krks.09@gmail.com>
2021-10-30 13:29:52 +0300

    Update home_page.dart

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:natore_project/page/sohbet.dart';
 import 'package:natore_project/provider/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
 import '../main.dart';
+
+User? user = null;
 
 class googleLoginPage extends StatelessWidget {
   @override
@@ -15,6 +19,7 @@ class googleLoginPage extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasData) {
+            print("logged in");
             return LoggedInWidget(); // giris yapılmış
           } else if (snapshot.hasError) {
             return Center(child: Text('Something went wrong!'));
@@ -29,7 +34,7 @@ class googleLoginPage extends StatelessWidget {
 class LoggedInWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser!;
+    user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromRGBO(100, 10, 23, 1),
@@ -41,9 +46,7 @@ class LoggedInWidget extends StatelessWidget {
                 'Logout',
               ),
               onPressed: () {
-                final provider =
-                    Provider.of<GoogleSignInProvider>(context, listen: false);
-                provider.logout();
+                logOut(context);
               },
             )
           ],
@@ -61,20 +64,45 @@ class LoggedInWidget extends StatelessWidget {
               SizedBox(height: 32),
               CircleAvatar(
                 radius: 40,
-                backgroundImage: NetworkImage(user.photoURL!),
+                backgroundImage: NetworkImage(user!.photoURL!),
               ),
               SizedBox(height: 8),
               Text(
-                'Isim: ' + user.displayName!,
+                'Isim: ' + user!.displayName!,
                 style: TextStyle(color: Colors.white, fontSize: 16),
               ),
               SizedBox(height: 8),
               Text(
-                'Email: ' + user.email!,
+                'Email: ' + user!.email!,
                 style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                child: Row(
+                  // Replace with a Row for horizontal icon + text
+                  children: <Widget>[
+                    FaIcon(FontAwesomeIcons.mailBulk, color: Colors.red),
+                    Text("  Sohbet")
+                  ],
+                ),
+                //const Text("Login"),
+                //icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Sohbet()),
+                  );
+                },
               ),
             ],
           ),
         ));
   }
 }
+
+void logOut(BuildContext context) {
+  final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
+  provider.logout();
+  user = null;
+}
+
